86a4036a1bdaaae4ab70c9edee4ca5259d23ea23
Maksim <bottleofjuice@yandex.ru>
2026-02-05 20:41:13 +0300

    first version of model

---
+++ model.py
@@ -0,0 +1,52 @@
+from torch import nn
+import torch
+
+
+class SASRec(nn.Module):
+    def __init__(self, num_embedding: int,
+                 seq_len: int,
+                 embedding_dim: int = 256,
+                 num_heads: int = 4,
+                 num_layers: int = 4,
+                 transformer_dim = 2048,
+                 transformer_dropout = 0.1):
+        super(SASRec, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.num_heads = num_heads
+        self.num_layers = num_layers
+
+        self.input_embedding = nn.Embedding(num_embeddings=num_embedding + 1, embedding_dim=embedding_dim)
+        self.position_embedding = nn.Embedding(num_embeddings=seq_len, embedding_dim=embedding_dim)
+
+        self.transformer = nn.TransformerEncoder(
+            encoder_layer=nn.TransformerEncoderLayer(
+                d_model=embedding_dim,
+                nhead=num_heads,
+                dim_feedforward=transformer_dim,
+                dropout=transformer_dropout,
+                batch_first=True
+            ),
+            num_layers=num_layers
+        )
+
+        self.pad_id = num_embedding
+
+    def forward(self, x):
+        input_embeddings = self.input_embedding(x)
+
+        seq_len = input_embeddings.shape[1]
+
+        positions = (torch.arange(seq_len, dtype=torch.long, device=input_embeddings.device)
+                     .unsqueeze(0).expand(x.size(0), seq_len))
+        position_embeddings = self.position_embedding(positions)
+
+        embeddings = input_embeddings + position_embeddings
+
+        mask = (x == self.pad_id)
+
+        casual_mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool()
+
+        attention = self.transformer(embeddings, mask=casual_mask,
+                                     src_key_padding_mask=mask)
+
+        return attention
