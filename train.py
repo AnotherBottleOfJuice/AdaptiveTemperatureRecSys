bb0a511a19528397b1de44d116dc32cc26290400
Maksim <bottleofjuice@yandex.ru>
2026-03-09 20:18:37 +0300

    new version from homework

--- train.py
+++ train.py
@@ -1,129 +1,87 @@
-import matplotlib.pyplot as plt
+import time
+import tqdm
 import torch
-from torch.nn.functional import cross_entropy
-from tqdm.notebook import tqdm
-
-from model import SASRec
+from torch.utils.tensorboard import SummaryWriter
 
 
-def show_metrics(metric1, metric2, label1, label2, name):
-    plt.figure(num=name)
-    plt.plot(range(len(metric1)), metric1, label=label1)
-    plt.plot(range(len(metric2)), metric2, label=label2)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+def train_loop(
+        graph,
+        train_dataloader,
+        log_dir: str,
+        optimizer,
+        scheduler=None,
+        num_epochs: int = 1,
+        grad_accum_steps: int = 1,
+        grad_clip: float = 0.0,
+        eval_every: int = -1,
+        validation_func=None,
+):
+    graph.train()
+    writer = SummaryWriter(log_dir=log_dir)
 
+    tokens_passed = 0
+    global_step = 0
 
-def train_epoch(model: SASRec, train_loader, optimizer):
-    model.train()
-    sum_loss = 0
+    for epoch in range(num_epochs):
+        train_it = iter(train_dataloader)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # all_embeddings = model.output_embedding.weight  # V, E
+        num_batches = len(train_dataloader)
 
-    for i, (positives, negatives) in enumerate(tqdm(train_loader, desc="Training")):
-        optimizer.zero_grad()
-        positives, negatives = positives.to(device), negatives.to(device)
+        for batch_idx in tqdm.tqdm(range(num_batches), total=num_batches, desc=f"epoch {epoch}"):
 
-        model_input = positives[:, :-1]  # B, S, E
-        positives = positives[:, 1:]
-        neg_embeddings = model.output_embedding(negatives)
-        pos_embeddings = model.output_embedding(positives)
+            last_step = (epoch == num_epochs - 1) and (batch_idx == num_batches - 1)
 
-        output = model(model_input)  # B, S, E
-        # print(neg_embeddings.shape)
-        neg_logits = torch.matmul(output, neg_embeddings.T)
-        pos_logits = (output * pos_embeddings).sum(dim=-1, keepdim=True)
-        # all_logits = torch.matmul(output, all_embeddings.t())
+            if validation_func is not None and eval_every != -1:
+                if global_step % eval_every == 0 or last_step:
+                    graph.eval()
+                    with torch.inference_mode():
+                        metrics = validation_func()
+                    graph.train()
 
-        logits = torch.cat([pos_logits, neg_logits], dim=-1)
-        logits = logits.reshape(logits.size(0) * logits.size(1), -1)
-        loss = cross_entropy(logits, torch.zeros(logits.shape[0],
-                                                 device=logits.device,
-                                                 dtype=torch.long))
+                    for name, value in metrics.items():
+                        if torch.is_tensor(value):
+                            value = float(value.detach().cpu())
+                        writer.add_scalar(f"valid/{name}", value, tokens_passed)
 
-        # loss = cross_entropy(all_logits.flatten(end_dim=1), positives.flatten())
+            torch.cuda.synchronize()
+            train_step_t0 = time.time()
 
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
 
-        sum_loss += loss.item()
+            train_loss = 0.0
+            curr_tokens = 0
 
-    return sum_loss / len(train_loader)
+            for _ in range(grad_accum_steps):
+                batch = next(train_it)
+                curr_tokens += batch.size
 
+                with torch.autocast("cuda", torch.bfloat16):
+                    loss = graph(batch)
 
-@torch.no_grad()
-def validate_epoch(model: SASRec, val_loader):
-    model.eval()
+                loss = loss / grad_accum_steps
+                train_loss += float(loss.detach())
+                loss.backward()
 
-    losses = []
-    top100_correct = 0
-    top10_correct = 0
-    total = 0
+            tokens_passed += curr_tokens
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    all_embeddings = model.output_embedding.weight  # V, E
+            writer.add_scalar("train/loss", train_loss, tokens_passed)
 
-    for labels in tqdm(val_loader, desc="Validation"):
-        labels = labels.to(device)
+            if grad_clip > 0.0:
+                grad_norm = torch.nn.utils.clip_grad_norm_(graph.parameters(), grad_clip)
+                writer.add_scalar("optim/grad_norm", float(grad_norm.detach().cpu()), tokens_passed)
 
-        model_input = labels[:, :-1]
-        targets: torch.Tensor = labels[:, 1:]
+            optimizer.step()
+            if scheduler is not None:
+                scheduler.step()
+                writer.add_scalar("optim/lr", optimizer.param_groups[0]["lr"], tokens_passed)
 
-        model_output: torch.Tensor = model(model_input)  # B, S, E
+            torch.cuda.synchronize()
+            train_step_t1 = time.time()
 
-        model_output_flat = model_output.flatten(end_dim=1)  # B*S, E
-        targets_flat = targets.flatten()  # B*S
+            dt = train_step_t1 - train_step_t0
+            writer.add_scalar("time/train_step_time(s)", dt, tokens_passed)
+            writer.add_scalar("time/tokens_per_sec(k)", (curr_tokens / dt) // 1000, tokens_passed)
 
-        mask = (targets_flat != model.pad_id)  # B*S
+            global_step += 1
 
-        if sum(mask) == 0:
-            continue
-
-        valid_output = model_output_flat[mask]  # N, E
-        valid_targets = targets_flat[mask]  # N
-
-        logits = torch.matmul(valid_output, all_embeddings.t())  # N, V
-        loss = cross_entropy(logits, valid_targets)
-        losses.append(loss.item())
-
-        top100_indices = torch.topk(logits, k=100, dim=-1).indices  # N, 10
-        top10_indices = top100_indices[:, :10]  # N, 10
-
-        top10_correct += (top10_indices == valid_targets.unsqueeze(-1)).any(dim=-1).sum().item()
-        top100_correct += (top100_indices == valid_targets.unsqueeze(-1)).any(dim=-1).sum().item()
-
-        total += mask.sum().item()
-
-    avg_loss = sum(losses) / len(losses)
-    top10_acc = top10_correct / total
-    top100_acc = top100_correct / total
-
-    return avg_loss, top100_acc, top10_acc
-
-
-def train(model: SASRec, train_loader, val_loader, optimizer,
-          epochs, scheduler=None):
-    train_losses, val_losses = [], []
-    top100_accs = []
-    top10_accs = []
-
-    for epoch in tqdm(range(epochs)):
-        train_losses.append(train_epoch(model, train_loader, optimizer))
-
-        sum_loss, top100_acc, top10_acc = validate_epoch(model, val_loader)
-
-        val_losses.append(sum_loss)
-        top100_accs.append(top100_acc)
-        top10_accs.append(top10_acc)
-
-        print(f"Epoch {epoch}: train_loss={train_losses[-1]:.4f}, val_loss={val_losses[-1]:.4f}, "
-              f"top10={top10_acc:.4f}, top100={top100_acc:.4f}")
-
-        show_metrics(train_losses, val_losses, "train", "val", "losses")
-        show_metrics(top100_accs, top10_accs, "top-100", "top-10", "accuracy")
-
-        if scheduler is not None:
-            scheduler.step()
+    writer.close()
