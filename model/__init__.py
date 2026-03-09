18cf80007e426fb9d0ae57e2eefba8aa2bd80d2d
Maksim <bottleofjuice@yandex.ru>
2026-03-09 20:09:30 +0300

    new verison of model from homework

---
+++ model/__init__.py
@@ -0,0 +1,13 @@
+from .model.graph import Graph
+from .model.gpt import GPT
+from .model.mlp import MLP
+from .model.block import Block
+from .model.causal_self_attension import CausalSelfAttention
+
+__all__ = [
+    "Graph",
+    "Block",
+    "MLP",
+    "CausalSelfAttention",
+    "GPT",
+]
