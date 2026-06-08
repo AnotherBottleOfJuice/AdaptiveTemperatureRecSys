056a7c1eecada2766835d5460c2750860534c74b
Maksim <bottleofjuice@yandex.ru>
2026-06-08 10:46:15 +0300

    30e linear result

--- submit.sh
+++ submit.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=linear_tau_30e
 #SBATCH --account=proj_1876
-#SBATCH --time=00:10:00
+#SBATCH --time=15:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus=1
+#SBATCH --gpus=2
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=maanbessolitsyn@edu.hse.ru
-#SBATCH --partition=test
 
 module load Python
 source activate adaptivetemperaturerecsys
