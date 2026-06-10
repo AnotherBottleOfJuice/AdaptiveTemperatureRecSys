13ca89c5608b85c819ed46e2d0451c9986829249
Maksim <bottleofjuice@yandex.ru>
2026-06-10 17:55:14 +0300

    updated configs

--- submits/test_parallel.sh
+++ submits/test_parallel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=constant_tau_30e
+#SBATCH --job-name=test
 #SBATCH --account=proj_1876
-#SBATCH --time=15:00:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=10
 #SBATCH --gpus=2
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=maanbessolitsyn@edu.hse.ru
 
 module load Python
 source activate adaptivetemperaturerecsys
-python main.py configs/constant_tau_30e.yaml
+python main.py configs/test.yaml
