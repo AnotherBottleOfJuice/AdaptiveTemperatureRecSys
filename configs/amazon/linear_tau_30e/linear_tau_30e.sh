44944ab6824449ce3343196a348aa788dabe297d
Maksim <bottleofjuice@yandex.ru>
2026-06-23 08:19:08 +0300

    finally cos + linear

--- configs/amazon/linear_tau_30e/linear_tau_30e.sh
+++ configs/amazon/linear_tau_30e/linear_tau_30e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=amazon_linear_tau_30e
 #SBATCH --account=proj_1876
-#SBATCH --time=26:00:00
+#SBATCH --time=36:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
 #SBATCH --gpus=2
