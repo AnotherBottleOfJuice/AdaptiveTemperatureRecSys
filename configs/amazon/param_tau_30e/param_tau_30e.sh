1a2f1942f33e6390448422898dad7e16ff522313
Maksim <bottleofjuice@yandex.ru>
2026-06-25 09:44:02 +0300

    updated configs

---
+++ configs/amazon/param_tau_30e/param_tau_30e.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=amazon_param_tau_30e
+#SBATCH --account=proj_1876
+#SBATCH --time=18:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=2
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=maanbessolitsyn@edu.hse.ru
+
+module load Python
+source activate adaptivetemperaturerecsys
+python main.py configs/amazon/param_tau_30e/param_tau_30e.yaml
